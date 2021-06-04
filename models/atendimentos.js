25571f94904b6dc646d7887ec53d28d13bf9a8e7
AllanRanier <allanraier18@gmail.com>
2021-06-04 16:44:58 -0300

    Adicionando, editando e excluido

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,97 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'cliente deve ser pelo menos cinco caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento,dataCriacao, data}
+
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(atendimento)
+                }
+            })
+        }
+
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos'
+        conexao.query(sql,(erro, resultados) => {
             if(erro) {
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(resultados)
+                res.status(201).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if (erro) {
+                res.status(400).json(erro)
+            }else{
+                res.status(201).json(atendimento)
+            }
+        })
+    }
+
+    alterar(id, valores, res){
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores,id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(resultados)
+            }else{
+                res.status(200).json({id})
             }
         })
     }
